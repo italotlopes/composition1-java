110947055d60a06c3f60ce60503184bd542104b1
Italo Lopes <italotlopes@gmail.com>
2021-11-03 20:17:53 -0300

    Added the main method and some corrections in classes

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,16 @@
  */
 package application;
 
+import entities.Department;
+import entities.HourContract;
+import entities.Worker;
+import entities.enums.WorkerLevel;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author HOME
@@ -13,9 +23,51 @@ public class Program {
 
     /**
      * @param args the command line arguments
+     * @throws java.text.ParseException
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws ParseException {
+        
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        
+        System.out.print("Enter department`s name: ");
+        String departmentName = input.nextLine();
+        System.out.println("Enter worker data: ");
+        System.out.print("Name: ");
+        String workerName = input.nextLine();
+        System.out.print("Level: ");
+        String workerLevel = input.nextLine();
+        System.out.print("Base salary: ");
+        double baseSalary = input.nextDouble();
+        Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), baseSalary,new Department(departmentName));
+        
+        System.out.println("========================================");
+        System.out.print("How many contracts to this worker? ");
+        int n = input.nextInt();
+        
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Enter contract #" + i + " data: ");
+            System.out.print("Date (DD/MM/YYYY): ");
+            Date contractDate = sdf.parse(input.next());
+            System.out.print("Value per hour: ");
+            double valuePerHour = input.nextDouble();
+            System.out.print("Duration (hours): ");
+            int hours = input.nextInt();
+            HourContract contract = new HourContract(contractDate, valuePerHour, hours);
+            worker.addContract(contract);
+        }
+        
+        System.out.println("========================================");
+        System.out.println("Enter month and year to calculate income (MM/YYYY): ");
+        String monthAndYear = input.next();
+        int month = Integer.parseInt(monthAndYear.substring(0, 2));
+        int year = Integer.parseInt(monthAndYear.substring(3));
+        
+        System.out.println("========================================");
+        System.out.println("Name: " + worker.getName());
+        System.out.println("Department: " + worker.getDepartment().getName());
+        System.out.println("Income for " + monthAndYear + ": " + String.format("%.2f", worker.income(year, month)));
     }
     
 }
--- src/entities/Department.java
+++ src/entities/Department.java
@@ -19,6 +19,14 @@ public class Department {
     public Department(String name) {
         this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
     
     
 }
